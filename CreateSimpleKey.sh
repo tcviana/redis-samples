f3640f7d723afc9d8a1f3077e2b838707cbacac9
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2020-12-29 08:32:05 -0300

    Create CreateSimpleKey.sh
    Criação, edição, consulta, visualização do tipo do valor

---
+++ CreateSimpleKey.sh
@@ -0,0 +1,17 @@
+# Criar chave simples, chave e valor
+SET nome Tiago
+
+# Visualizar o valor chave
+GET nome
+
+# Atualização da chave
+SET nome "Tiago Viana"
+
+# Confirmar alteração
+GET nome
+
+# Ver o tipo do valor da chave (string)
+TYPE nome
+
+# Remover chave
+DEL nome
