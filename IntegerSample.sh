22fcc318c1e7d419587f2dc4231a27b4b292ab26
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2020-12-29 10:01:45 -0300

    Update IntegerSample.sh
    Adicionado incrbyfloat e decrby

--- IntegerSample.sh
+++ IntegerSample.sh
@@ -1,5 +1,5 @@
 # Criação da chave 
-SET numero 0
+SET numero 1
 
 # Visualizar o valor
 GET numero 
@@ -7,14 +7,21 @@ GET numero
 # Incrementar valor, default 1
 INCR numero
 
+# Decrementar valor, default 1
+DECR numero
+
 # Confirmar incremento
 GET numero
 
 # Incrementar 15
 INCRBY numero 15
 
+# Incrementar decimal
+INCRBYFLOAT numero 7.5
+
 # Decrementar 10
 INCRBY numero -10
+DECRBY numero 2
 
 # Remover chave
 DEL numero
